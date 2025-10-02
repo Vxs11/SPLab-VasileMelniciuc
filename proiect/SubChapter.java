2ed716c8783e438d997292508f48f7f63a1e1275
Vxs11 <vasile.melniciuc05@e-uvt.ro>
2025-10-02 09:49:40 +0000

    lab0final

---
+++ proiect/SubChapter.java
@@ -0,0 +1,10 @@
+public class SubChapter {
+private String name;
+ 
+ public SubChapter(String name){
+    this.name=name;
+ }
+ public print(){
+    System.out.println(name);
+ }
+}
---
+++ proiect/Table.java
@@ -0,0 +1,10 @@
+public class Table {
+private String title;
+ 
+ public Author(String title){
+    this.title=title;
+ }
+ public print(){
+    System.out.println(title);
+ }
+}
