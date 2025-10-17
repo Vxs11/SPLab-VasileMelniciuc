cc2d1b58a11463a82b8bbf2594848563d397ce1e
Vxs11 <vasile.melniciuc05@e-uvt.ro>
2025-10-17 08:32:55 +0000

    lab 2

--- proiect/Paragraph.java
+++ proiect/Paragraph.java
@@ -1,10 +1,27 @@
-public class Paragraph {
-private String text;
- 
- public Paragraph(String text){
-    this.text=text;
- }
- public print(){
-    System.out.println(text);
- }
+public class Paragraph implements Element {
+    private String text;
+
+    public Paragraph(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public void print() {
+        System.out.println("Paragraph: " + text);
+    }
+
+    @Override
+    public void add(Element element) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void remove(Element element) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Element get(int index) {
+        throw new UnsupportedOperationException();
+    }
 }
